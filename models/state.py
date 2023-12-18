db9f5934b1b232294eb49a5303f895c3624fa7e6
Kenrique _Ngwa <72971484+kenrique100@users.noreply.github.com>
2023-12-18 18:37:05 -0500

    updated models

--- models/state.py
+++ models/state.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
 
+class State(BaseModel, Base):
+    """Representation of state """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128),
+                      nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                              backref="states")
+    else:
+        name = ""
 
-class State(BaseModel):
-    """Class for managing state objects"""
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
 
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """fs getter attribute that returns City instances"""
+            values_city = models.storage.all("City").values()
+            list_city = []
+            for city in values_city:
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
